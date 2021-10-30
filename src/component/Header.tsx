0a5c4102139d12374a09c13e8102e8718318f22d
hunman89 <hunman89@gmail.com>
2021-10-30 19:30:27 +0900

    add. tab css

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 
 export const Header = () => {
-    return (
-        <div className="justify-center text-center py-10">
-            <p className="inline text-4xl font-bold text-green-700 px-2">
-                농축산업
-            </p>
-            <p className="inline text-4xl text-gray-500 px-2">
-                경영정보 제공서비스
-            </p>
-        </div>
-    );
+  return (
+    <div className="justify-center text-center py-10">
+      <p className="inline text-4xl font-bold text-green-700 px-2">
+        농 &#x000B7; 축산업
+      </p>
+      <p className="inline text-4xl text-gray-500 px-2">경영정보 제공서비스</p>
+    </div>
+  );
 };
--- src/component/Tab.tsx
+++ src/component/Tab.tsx
@@ -1,12 +1,12 @@
 export const Tab = () => {
-    return (
-        <div className="bg-green-500 px-10 py-3">
-            <p className="inline text-xl font-semibold text-gray-600 px-5">
-                한우
-            </p>
-            <p className="inline text-xl font-semibold text-gray-600 px-5">
-                기타
-            </p>
-        </div>
-    );
+  return (
+    <div className="flex justify-center bg-green-500 px-10 py-3">
+      <div className="inline text-xl font-semibold text-gray-600 px-5 hover:text-black">
+        한우
+      </div>
+      <div className="inline text-xl font-semibold text-gray-600 px-5 hover:text-black">
+        기타
+      </div>
+    </div>
+  );
 };
