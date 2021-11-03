ff9404bdfa60c698d5c35ca3a552ae0dfb2d7f2f
hunman89 <hunman89@gmail.com>
2021-11-03 21:38:34 +0900

    add. 블로그 링크

--- src/component/Tab.tsx
+++ src/component/Tab.tsx
@@ -1,12 +1,19 @@
 export const Tab = () => {
+  const goToBlog = () => {
+    window.open("https://nonguplab.tistory.com/");
+  };
+
   return (
     <div className="flex justify-center bg-green-500 px-10 py-3">
-      <div className="inline text-xl font-semibold text-gray-600 px-5 hover:text-black">
+      <button className="inline text-xl font-semibold text-gray-600 px-5 hover:text-black">
         한우
-      </div>
-      <div className="inline text-xl font-semibold text-gray-600 px-5 hover:text-black">
-        기타
-      </div>
+      </button>
+      <button
+        className="inline text-xl font-semibold text-gray-600 px-5 hover:text-black"
+        onClick={(e) => goToBlog()}
+      >
+        블로그
+      </button>
     </div>
   );
 };
