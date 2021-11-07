880a71d81f26874c0cb7558e228bbafdfe952899
hunman89 <hunman89@gmail.com>
2021-11-07 17:11:39 +0900

    fix. 오타 수정

--- src/component/Footer.tsx
+++ src/component/Footer.tsx
@@ -2,7 +2,7 @@ export const Footer = () => {
   return (
     <div className="justify-center text-center bg-gray-300 p-24">
       <p className=" text-2xl text-gray-500 py-8">nonguplab.com</p>
-      <p>Coptright 2021. NALCEED Corp. all right reserved.</p>
+      <p>Coptright 2021. NALSEED Corp. all right reserved.</p>
     </div>
   );
 };
